86603161e1a8a635a26c89c5b36e046e7642a92c
ArturoSantos06 <artsantoss30@gmail.com>
2025-11-26 13:12:24 -0600

    Fix: Subir imágenes y configurar URLs para servir media files en producción

--- gym/urls.py
+++ gym/urls.py
@@ -4,6 +4,7 @@ from django.views.generic import TemplateView
 from rest_framework.authtoken import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve  # <--- Importar serve para producción
 
 # 1. Rutas principales de Django y API
 urlpatterns = [
@@ -11,14 +12,17 @@ urlpatterns = [
     path('api/', include('gymapp.urls')),
     
     # Ruta para Login (Token)
-    path('api-token-auth/', views.obtain_auth_token), 
+    path('api-token-auth/', views.obtain_auth_token),
+    
+    # 2. Servir archivos media en producción (Render)
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
-# 2. Configuración para servir Imágenes en Desarrollo (Media)
+# 3. Configuración para servir Imágenes en Desarrollo (Media)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# 3. Ruta Catch-all para React
+# 4. Ruta Catch-all para React
 urlpatterns += [
     re_path(r'^.*$', TemplateView.as_view(template_name='base.html')),
 ]
